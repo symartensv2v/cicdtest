6969ad08b91fc9e80f51ecff07daa719fcd65dd7
symartensv2v <symartens@v2v.ca>
2022-04-04 18:26:51 -0400

    Create demo2.php

---
+++ demo2.php
@@ -0,0 +1,17 @@
+<?php
+function test()
+{
+    print_r('dsadsadsad');
+}
+$name = "Quincy";
+echo "Hi! My name is " . $name . "<br>";
+echo "Hi! My name is " . $NAME . "<br>";
+echo "Hi! My name is " . $NaMe . "<br>";
+echo "Hi! My name is " . $name . "<br>";
+echo "Hi! My name is " . $NAME . "<br>";
+echo "Hi! My name is " . $NaMe . "<br>";
+test();
+echo "Hi! My name is " . $name . "<br>";
+echo "Hi! My name is " . $NAME . "<br>";
+echo "Hi! My name is " . $NaMe . "<br>";
+?>
