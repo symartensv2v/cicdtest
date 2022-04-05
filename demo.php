27180c0691d4dfb8409e0f7af6e42ffba0f8273e
symartensv2v <symartens@v2v.ca>
2022-04-04 20:29:05 -0400

    Update demo.php

--- demo.php
+++ demo.php
@@ -8,4 +8,14 @@ echo "Hi! My name is " . $name . "<br>";
 echo "Hi! My name is " . $NAME . "<br>";
 echo "Hi! My name is " . $NaMe . "<br>";
 test();
+test();
+test23();
+$test = mysqli_connect('', '', '');
+test23();
+$test = mysqli_close('', '', '');
+test();
+test23();
+$test = mysqli_connect('', '', '');
+test23();
+$test = mysqli_close('', '', '');
 ?>
