82438db08707dbae32ade264f2bb873c32e46e92
symartensv2v <symartens@v2v.ca>
2022-04-05 13:18:05 -0400

    ...

---
+++ core/databases/connections/cdatadb.php
@@ -0,0 +1,11 @@
+<?php
+$test = new mysqli();
+test23();
+$test = mysqli_connect('', '', '');
+test23();
+$test = mysqli_close('', '', '');
+test();
+test23();
+$test = mysqli_connect('', '', '');
+test23();
+$test = mysqli_close('', '', '');
