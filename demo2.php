6d64f6356ec37f9701aa8602f028008cbed5ada9
Sylvain Martens <sylvainmartens@gmail.com>
2022-04-05 01:05:18 -0400

    Update demo2.php

--- demo2.php
+++ demo2.php
@@ -3,3 +3,13 @@ function test()
 {
     print_r('dsadsadsad');
 }
+$name = "Quincy";
+echo "test";
+echo "Hi! My name is " . $name . "<br>";
+echo "Hi! My name is " . $NAME . "<br>";
+echo "Hi! My name is " . $NaMe . "<br>";
+echo "Hi! My name is " . $name . "<br>";
+echo "Hi! My name is " . $NAME . "<br>";
+echo "Hi! My name is " . $NaMe . "<br>";
+test();
+?>
